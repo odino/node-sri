be57ff6fcceb8eb87cc64d50d74750b39f04575d
odino <alessandro.nadalin@gmail.com>
2015-09-26 21:10:54 +0400

    Support different algorithms and prefix hashes with the algo's name

--- test.js
+++ test.js
@@ -9,13 +9,30 @@ describe('sri', function() {
 
     it('should return an hash when generating an sri for an existing file', function (done) {
       sri.hash('fixtures/sample.js').then(function(hash){
-        assert.strictEqual('OLBgp1GsljhM2TJ+sbHjaiH9txEUvgdDTAzHv2P24donTt6/529l+9Ua0vFImLlb', hash)
+        assert.strictEqual('sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=', hash)
         done()
       }).catch(function(err){
         console.log(err)
       })
     });
 
+    it('should accept an option to not return the algo in the hash', function (done) {
+      sri.hash({file: 'fixtures/sample.js', prefix: false}).then(function(hash){
+        assert.strictEqual('47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=', hash)
+        done()
+      }).catch(function(err){
+        console.log(err)
+      })
+    });
+
+    it('should accept an option to use different algorithms', function (done) {
+      sri.hash({file: 'fixtures/sample.js', algo: 'sha512'}).then(function(hash){
+        assert.strictEqual('sha512-z4PhNX7vuL3xVChQ1m2AB9Yg5AULVxXcg/SpIdNs6c5H0NE8XYXysP+DGNKHfuwvY7kxvUdBeoGlODJ6+SfaPg==', hash)
+        done()
+      }).catch(function(err){
+        console.log(err)
+      })
+    });
 
     it('should return an error when generating an sri for a non existing file', function (done) {
       sri.hash('fixtures/nooooooo.js').catch(function(err){
@@ -30,7 +47,7 @@ describe('sri', function() {
 
     it('should return an hash when generating an sri for an existing file with a callback', function (done) {
       sri.hash('fixtures/sample.js', function(err, hash){
-        assert.strictEqual('OLBgp1GsljhM2TJ+sbHjaiH9txEUvgdDTAzHv2P24donTt6/529l+9Ua0vFImLlb', hash)
+        assert.strictEqual('sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=', hash)
         assert.strictEqual(null, err)
         done()
       })
